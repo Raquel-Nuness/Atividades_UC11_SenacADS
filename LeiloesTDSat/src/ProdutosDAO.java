0062e4b293e6f99c3da41622c08c366c1dedb3ed
Raquel-Nuness <raquelnunes.nr@gmail.com>
2024-07-27 19:13:44 -0300

    Projeto Inicial

---
+++ LeiloesTDSat/src/ProdutosDAO.java
@@ -0,0 +1,113 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Adm
+ */
+
+import java.sql.PreparedStatement;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+import java.sql.SQLException;
+
+
+public class ProdutosDAO {
+    
+    Connection conn;
+    PreparedStatement prep;
+    ResultSet resultset;
+    ArrayList<ProdutosDTO> listagem = new ArrayList<>();
+    
+    public boolean venderProduto(int id) {
+    conn = new conectaDAO().connectDB();
+    
+    try {
+        String sql = "UPDATE produtos SET status = 'Vendido' WHERE id = ?";
+        prep = conn.prepareStatement(sql);
+        prep.setInt(1, id);
+        prep.executeUpdate();
+        return true;
+    } catch (SQLException erro) {
+        JOptionPane.showMessageDialog(null, "ProdutosDAO Vender: " + erro);
+        return false;
+    }
+}
+
+    
+    
+    
+    public boolean cadastrarProduto (ProdutosDTO produto){
+conn = new conectaDAO().connectDB();
+    
+    try {
+        String sql = "INSERT INTO produtos (nome, valor, status) VALUES (?, ?, ?)";
+        prep = conn.prepareStatement(sql);
+        prep.setString(1, produto.getNome());
+        prep.setInt(2, produto.getValor());
+        prep.setString(3, produto.getStatus());
+        prep.execute();
+        return true;
+    } catch (SQLException erro) {
+        JOptionPane.showMessageDialog(null, "ProdutosDAO Cadastrar: " + erro);
+        return false;
+    }  
+    }
+    
+public ArrayList<ProdutosDTO> listarProdutosVendidos(){
+    conn = new conectaDAO().connectDB();
+    
+    try {
+        String sql = "SELECT * FROM produtos WHERE status = 'Vendido'";
+        prep = conn.prepareStatement(sql);
+        resultset = prep.executeQuery();
+        
+        while (resultset.next()) {
+            ProdutosDTO produto = new ProdutosDTO();
+            produto.setId(resultset.getInt("id"));
+            produto.setNome(resultset.getString("nome"));
+            produto.setValor(resultset.getInt("valor"));
+            produto.setStatus(resultset.getString("status"));
+            
+            listagem.add(produto);
+        }
+    } catch (SQLException erro) {
+        JOptionPane.showMessageDialog(null, "ProdutosDAO Listar Vendidos: " + erro);
+    }
+    
+    return listagem;
+}
+    public ArrayList<ProdutosDTO> listarProdutos(){
+        conn = new conectaDAO().connectDB();
+        
+        try {
+            String sql = "SELECT * FROM produtos";
+            prep = conn.prepareStatement(sql);
+            resultset = prep.executeQuery();
+            
+            while (resultset.next()) {
+                ProdutosDTO produto = new ProdutosDTO();
+                produto.setId(resultset.getInt("id"));
+                produto.setNome(resultset.getString("nome"));
+                produto.setValor(resultset.getInt("valor"));
+                produto.setStatus(resultset.getString("status"));
+                
+                listagem.add(produto);
+            }
+        } catch (SQLException erro) {
+            JOptionPane.showMessageDialog(null, "ProdutosDAO Listar: " + erro);
+        }
+        
+        return listagem;
+    }
+
+    
+    
+    
+        
+}
+
---
+++ LeiloesTDSat/src/conectaDAO.java
@@ -0,0 +1,36 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Adm
+ */
+public class conectaDAO {
+    
+    public Connection connectDB(){
+        Connection conn = null;
+        
+        try {
+            String url = "jdbc:mysql://localhost/uc11";
+            String user = "root";
+            String password = "Melambe69";
+            
+            conn = DriverManager.getConnection(url, user, password);
+            
+        } catch (SQLException erro){
+            JOptionPane.showMessageDialog(null, "Erro ConectaDAO: " + erro.getMessage());
+        }
+        return conn;
+    }
+    
+}
